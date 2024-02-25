ba44ab12bbf1f0aa26c9f82b054aa1a6adfdfc1a
Bailey Ellison <beeusc2018@gmail.com>
2024-02-25 15:42:47 -0500

    Added Arguments to JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,32 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword(length, includeLowercase, includeUppercase, includeSymbols, includeNumbers) {
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
+  const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberCharacters = "0123456789"
+  const symbolCharacters = " !@#$%&*'()+-,./:;<>=?{}[]~_`|"
+
+  // if(length > 8)
+
+  return '';
 }
 
+// Arguments to pass
+const passwordLength = 16;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeSymbols = true;
+const includeNumbers = true;
+
+// Calling the function
+const generatePassword = writePassword(length, includeLowercase, includeUppercase, includeSymbols, includeNumbers);
+
+document.write(generatePassword)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
