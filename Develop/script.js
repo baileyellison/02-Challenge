9b70f5997169298627e7fd85618fefa2479bd202
Bailey Ellison <beeusc2018@gmail.com>
2024-02-25 17:10:55 -0500

    Changed JavaScript to include if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword(length, includeLowercase, includeUppercase, includeSymbols, includeNumbers) {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-  const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-  const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numberCharacters = "0123456789";
-  const symbolCharacters = " !@#$%&*'()+-,./:;<>=?{}[]~_`|";
-
-  let allowedCharacters = "";
-  let password = "";
-
-  allowedCharacters += includeLowercase ? lowercaseCharacters : "";
-  allowedCharacters += includeUppercase ? uppercaseCharacters : "";
-  allowedCharacters += includeNumbers ? numberCharacters : "";
-  allowedCharacters += includeSymbols ? symbolCharacters : "";
-
-  
-  if(length <= 8) {
-    return '(Password length must be at least 8 characters.)'
+  const types = { 
+    lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    numberCharacters = "0123456789";
+    symbolCharacters = " !@#$%&*'()+-,./:;<>=?{}[]~_`|";
   }
 
-  return '';
+  const getTypes = {
+    function lowercaseCharacters() {
+      return types.lowercaseCharacters[Math.floor(Math.random() * types.lowercaseCharacters.length)];
+    }
+  }
+
+  const getTypes = {
+    function uppercaseCharacters() {
+      return types.uppercaseCharacters[Math.floor(Math.random() * types.uppercaseCharacters.length)];
+    }
+  }
+
+  const getTypes = {
+    function numberCharacters() {
+      return types.numberCharacters[Math.floor(Math.random() * types.numberCharacters.length)];
+    }
+  }
+
+  const getTypes = {
+    function symbolCharacters() {
+      return types.symbolCharacters[Math.floor(Math.random() * types.symbolCharacters.length)];
+    }
+  }
+
+  // Write password to the #password input
+function writePassword() {
+  let uppercase = document.getElementById("Uppercase").checked;
+  let lowercase = document.getElementById("lowercase").checked;
+  let numbers= document.getElementById("numbers").checked;
+  let symbols = document.getElementById("symbols").checked;
+
+  if(numbers + symbols + lowercase + uppercase === 0) {
+    window.alert("No characters have been chosen to generate your password. Please check at least one box")
+  }
+
+  let passwordText = document.getElementById("password");
+  let length = document.getElementById("length");
+  let password = "";
+
+  if(length <= 7 + length >= 129) {
+    window.alert("Password length must be between 8 and 128 characters.")
+  }
+
+  let includedCharacters = getType[Math.floor(Math.random() * getType.length)];
+  let checkedCharacters = document.getElementById(includedCharacters.name).checked;
+
+  if(checkedCharacters) {
+    password += includedCharacters();
+  }
+  document.writePassword
 }
+
 
 // Arguments to pass
 const passwordLength = 8;
@@ -36,9 +68,6 @@ const includeUppercase = true;
 const includeSymbols = true;
 const includeNumbers = true;
 
-// Calling the function
-const generatePassword = writePassword(length, includeLowercase, includeUppercase, includeSymbols, includeNumbers);
 
-document.write(generatePassword)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
